d522f6553b6c5687cbd67077c17fe863160262ce
Amani Bouzid <160531428+atigRecruiting@users.noreply.github.com>
2024-03-05 11:35:14 +0100

    Update process-form.service.ts

--- src/main/webapp/app/entities/process/update/process-form.service.ts
+++ src/main/webapp/app/entities/process/update/process-form.service.ts
@@ -1,66 +1,66 @@
-// import { Injectable } from '@angular/core';
-// import { FormGroup, FormControl, Validators } from '@angular/forms';
-//
-// import { IProcess, NewProcess } from '../process.model';
-//
-// /**
-//  * A partial Type with required key is used as form input.
-//  */
-// type PartialWithRequiredKeyOf<T extends { id: unknown }> = Partial<Omit<T, 'id'>> & { id: T['id'] };
-//
-// /**
-//  * Type for createFormGroup and resetForm argument.
-//  * It accepts IProcess for edit and NewProcessFormGroupInput for create.
-//  */
-// type ProcessFormGroupInput = IProcess | PartialWithRequiredKeyOf<NewProcess>;
-//
-// type ProcessFormDefaults = Pick<NewProcess, 'id'>;
-//
-// type ProcessFormGroupContent = {
-//   id: FormControl<IProcess['id'] | NewProcess['id']>;
-//   title: FormControl<IProcess['title']>;
-//   type: FormControl<IProcess['type']>;
-// };
-//
-// export type ProcessFormGroup = FormGroup<ProcessFormGroupContent>;
-//
-// @Injectable({ providedIn: 'root' })
-// export class ProcessFormService {
-//   createProcessFormGroup(process: ProcessFormGroupInput = { id: null }): ProcessFormGroup {
-//     const processRawValue = {
-//       ...this.getFormDefaults(),
-//       ...process,
-//     };
-//     return new FormGroup<ProcessFormGroupContent>({
-//       id: new FormControl(
-//         { value: processRawValue.id, disabled: true },
-//         {
-//           nonNullable: true,
-//           validators: [Validators.required],
-//         },
-//       ),
-//       title: new FormControl(processRawValue.title),
-//       type: new FormControl(processRawValue.type),
-//     });
-//   }
-//
-//   getProcess(form: ProcessFormGroup): IProcess | NewProcess {
-//     return form.getRawValue() as IProcess | NewProcess;
-//   }
-//
-//   resetForm(form: ProcessFormGroup, process: ProcessFormGroupInput): void {
-//     const processRawValue = { ...this.getFormDefaults(), ...process };
-//     form.reset(
-//       {
-//         ...processRawValue,
-//         id: { value: processRawValue.id, disabled: true },
-//       } as any /* cast to workaround https://github.com/angular/angular/issues/46458 */,
-//     );
-//   }
-//
-//   private getFormDefaults(): ProcessFormDefaults {
-//     return {
-//       id: null,
-//     };
-//   }
-// }
+import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { IProcess, NewProcess } from '../process.model';
+
+/**
+ * A partial Type with required key is used as form input.
+ */
+type PartialWithRequiredKeyOf<T extends { id: unknown }> = Partial<Omit<T, 'id'>> & { id: T['id'] };
+
+/**
+ * Type for createFormGroup and resetForm argument.
+ * It accepts IProcess for edit and NewProcessFormGroupInput for create.
+ */
+type ProcessFormGroupInput = IProcess | PartialWithRequiredKeyOf<NewProcess>;
+
+type ProcessFormDefaults = Pick<NewProcess, 'id'>;
+
+type ProcessFormGroupContent = {
+  id: FormControl<IProcess['id'] | NewProcess['id']>;
+  title: FormControl<IProcess['title']>;
+  type: FormControl<IProcess['type']>;
+};
+
+export type ProcessFormGroup = FormGroup<ProcessFormGroupContent>;
+
+@Injectable({ providedIn: 'root' })
+export class ProcessFormService {
+  createProcessFormGroup(process: ProcessFormGroupInput = { id: null }): ProcessFormGroup {
+    const processRawValue = {
+      ...this.getFormDefaults(),
+      ...process,
+    };
+    return new FormGroup<ProcessFormGroupContent>({
+      id: new FormControl(
+        { value: processRawValue.id, disabled: true },
+        {
+          nonNullable: true,
+          validators: [Validators.required],
+        },
+      ),
+      title: new FormControl(processRawValue.title),
+      type: new FormControl(processRawValue.type),
+    });
+  }
+
+  getProcess(form: ProcessFormGroup): IProcess | NewProcess {
+    return form.getRawValue() as IProcess | NewProcess;
+  }
+
+  resetForm(form: ProcessFormGroup, process: ProcessFormGroupInput): void {
+    const processRawValue = { ...this.getFormDefaults(), ...process };
+    form.reset(
+      {
+        ...processRawValue,
+        id: { value: processRawValue.id, disabled: true },
+      } as any /* cast to workaround https://github.com/angular/angular/issues/46458 */,
+    );
+  }
+
+  private getFormDefaults(): ProcessFormDefaults {
+    return {
+      id: null,
+    };
+  }
+}
